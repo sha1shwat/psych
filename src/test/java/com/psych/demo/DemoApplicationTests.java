3dca2f72e4ab8328dbcd8923cfd047abe68c3f00
won1der <shas1hwat@gmail.com>
2019-12-21 19:23:52 +0530

    init

---
+++ src/test/java/com/psych/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psych.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
